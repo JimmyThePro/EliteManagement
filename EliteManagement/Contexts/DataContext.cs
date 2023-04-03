5a99db8e7f339590b950c1062b64ac4d7ac3dab9
Jimmy <jimmyjunttila@hotmail.com>
2023-04-03 13:48:47 +0200

    Create a 'local SqlServer database file' with my connection-string

---
+++ EliteManagement/Contexts/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EliteManagement.Contexts;
+
+internal class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected DataContext()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jimmy\source\repos\EliteManagement\EliteManagement\Contexts\elitemanagement_db.mdf;Integrated Security=True;Connect Timeout=30");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
